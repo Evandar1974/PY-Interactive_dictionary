065e90655ba27212a05325350201cd0c236affb1
Mark <markjcarroll@hotmail.co.uk>
2018-08-24 19:24:58 +0100

    convert text to lower case and check value exists

--- Interactive Dictionary/Interactive Dictionary/Interactive_Dictionary.py
+++ Interactive Dictionary/Interactive Dictionary/Interactive_Dictionary.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+	if word.lower() in data:
+		return data[word]
+	else:
+		return "The word does not exist please check it."
 
 word = input("Enter word: ")
 
