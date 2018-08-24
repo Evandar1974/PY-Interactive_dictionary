bfc02aa4f43abf5fb4b4dcc7d4a4643e74090672
Mark <markjcarroll@hotmail.co.uk>
2018-08-24 19:46:41 +0100

    code added to find close matches in dictionary

--- Interactive Dictionary/Interactive Dictionary/Interactive_Dictionary.py
+++ Interactive Dictionary/Interactive Dictionary/Interactive_Dictionary.py
@@ -1,12 +1,17 @@
 
 import json
+from difflib import get_close_matches
+
 data = json.load(open("data.json"))
 
 def translate(word):
 	if word.lower() in data:
 		return data[word]
+	elif len(get_close_matches(word, data.keys())) > 0:
+		return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
 	else:
 		return "The word does not exist please check it."
+
 
 word = input("Enter word: ")
 
