57975e65ba12820df5fbd3829efe60d17b466251
Mark <markjcarroll@hotmail.co.uk>
2018-08-25 19:30:45 +0100

    Added close matching and input to determine alternate word

--- Interactive Dictionary/Interactive Dictionary/Interactive_Dictionary.py
+++ Interactive Dictionary/Interactive Dictionary/Interactive_Dictionary.py
@@ -8,7 +8,14 @@ def translate(word):
 	if word.lower() in data:
 		return data[word]
 	elif len(get_close_matches(word, data.keys())) > 0:
-		return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+		yn = input("Did you mean %s instead? please enter Y or N: " % get_close_matches(word, data.keys())[0]).upper
+		if yn == "Y":
+			return data[get_close_matches(word, data.keys())[0]]
+		elif yn == "N":
+			return "The word doesnt exist. please double check it"
+		else:
+			return "We didnt understand your entry."
+
 	else:
 		return "The word does not exist please check it."
 
