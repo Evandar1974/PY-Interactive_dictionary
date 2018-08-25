2f3f860afb67b98f0fe5069c4ceb5d38ef14f2aa
Mark <markjcarroll@hotmail.co.uk>
2018-08-25 19:35:55 +0100

    added output refinements and dealing with strings and lists. comand line version of the program complete

--- Interactive Dictionary/Interactive Dictionary/Interactive_Dictionary.py
+++ Interactive Dictionary/Interactive Dictionary/Interactive_Dictionary.py
@@ -22,4 +22,11 @@ def translate(word):
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+	for item in output:
+		print(item)
+else:
+	print(output)
+
